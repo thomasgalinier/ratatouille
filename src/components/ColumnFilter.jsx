4266084cd8f1a4bc598a7db99919f5016a3f2a4b
Thomas Galinier <thomasgalinier@mbp-de-thomas.home>
2023-03-31 16:01:18 +0200

    calul + tri

---
+++ src/components/ColumnFilter.jsx
@@ -0,0 +1,15 @@
+export default function ColumnFilter({
+    column: { filterValue, preFilteredRows, setFilter },
+  }) {
+    const count = preFilteredRows.length;
+  
+    return (
+      <input
+        value={filterValue || ''}
+        onChange={(e) => {
+          setFilter(e.target.value || undefined); // Set undefined to remove the filter entirely
+        }}
+        placeholder={`Search ${count} records...`}
+      />
+    );
+  }
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,8 +1,8 @@
-
 import React from 'react';
-import { useSortBy, useTable } from 'react-table';
+import { useSortBy, useTable, useFilters } from 'react-table';
 import data from '../../data/variable.json';
 import '../table.css'
+import ColumnFilter from './ColumnFilter';
 function Table() {
  
     function colorGradient(value, minValue) {
@@ -17,14 +17,17 @@ function Table() {
             
           return cellStyle
       }
-
+  
+      
 
   const columns = React.useMemo(
     () => [
       {
         Header: 'Variable ID',
         accessor: 'var_id',
-      },
+        Filter: {ColumnFilter}
+           
+            },
       {
         Header: 'Variable Name',
         accessor: 'var_name',
@@ -33,8 +36,9 @@ function Table() {
         Header: 'Location',
         accessor: 'location',
       },
+      
       {
-        Header: 'D_10',
+        Header: 'd_10',
         accessor: 'd_10',
         Cell: ({ value }) => {
           const cellStyle = colorGradient(value, 80)
@@ -50,92 +54,108 @@ function Table() {
       
       
       {
-        Header: 'D_9',
+        Header: 'd_9',
         accessor: 'd_09',
         Cell: ({ value }) => {
             const cellStyle = colorGradient(value, 80)
             return <div style={cellStyle}>{value}</div>;
           },
-          sortType: 'basic', 
-        sorted: true, 
-        sortedDesc: false,
+          sortType: (a, b, column) => {
+            const aNum = parseFloat(a.original[column.accessor]);
+            const bNum = parseFloat(b.original[column.accessor]);
+            return aNum > bNum ? 1 : aNum < bNum ? -1 : 0;
+          },
       },
       {
-        Header: 'D_8',
+        Header: 'd_8',
         accessor: 'd_08',
         Cell: ({ value }) => {
             const cellStyle = colorGradient(value, 80)
             return <div style={cellStyle}>{value}</div>;
           },
-          sortType: 'basic', 
-        sorted: true, 
-        sortedDesc: false,
+          sortType: (a, b, column) => {
+            const aNum = parseFloat(a.original[column.accessor]);
+            const bNum = parseFloat(b.original[column.accessor]);
+            return aNum > bNum ? 1 : aNum < bNum ? -1 : 0;
+          },
       },
       {
-        Header: 'D_7',
+        Header: 'd_7',
         accessor: 'd_07',
         Cell: ({ value }) => {
             const cellStyle = colorGradient(value, 80)
             return <div style={cellStyle}>{value}</div>;
           },
-          sortType: 'basic', 
-        sorted: true, 
-        sortedDesc: false,
+          sortType: (a, b, column) => {
+            const aNum = parseFloat(a.original[column.accessor]);
+            const bNum = parseFloat(b.original[column.accessor]);
+            return aNum > bNum ? 1 : aNum < bNum ? -1 : 0;
+          },
       },
       {
-        Header: 'D_6',
+        Header: 'd_6',
         accessor: 'd_06',
         Cell: ({ value }) => {
             const cellStyle = colorGradient(value, 80)
             return <div style={cellStyle}>{value}</div>;
           },
-          sortType: 'basic', 
-        sorted: true, 
-        sortedDesc: false,
+          sortType: (a, b, column) => {
+            const aNum = parseFloat(a.original[column.accessor]);
+            const bNum = parseFloat(b.original[column.accessor]);
+            return aNum > bNum ? 1 : aNum < bNum ? -1 : 0;
+          },
       },
       {
-        Header: 'D_5',
+        Header: 'd_5',
         accessor: 'd_05',
         Cell: ({ value }) => {
             const cellStyle = colorGradient(value, 80)
             return <div style={cellStyle}>{value}</div>;
           },
-          sortType: 'basic', 
-        sorted: true, 
-        sortedDesc: false,
+          sortType: (a, b, column) => {
+            const aNum = parseFloat(a.original[column.accessor]);
+            const bNum = parseFloat(b.original[column.accessor]);
+            return aNum > bNum ? 1 : aNum < bNum ? -1 : 0;
+          },
       },
       {
-        Header: 'D_4',
+        Header: 'd_4',
         accessor: 'd_04',
         Cell: ({ value }) => {
             const cellStyle = colorGradient(value, 80)
             return <div style={cellStyle}>{value}</div>;
           },
-        sortType: 'basic', 
-        sorted: true, 
-        sortedDesc: false,
+          sortType: (a, b, column) => {
+            const aNum = parseFloat(a.original[column.accessor]);
+            const bNum = parseFloat(b.original[column.accessor]);
+            return aNum > bNum ? 1 : aNum < bNum ? -1 : 0;
+          },
       },
       {
-        Header: 'D_3',
+        Header: 'd_3',
         accessor: 'd_03',
         Cell: ({ value }) => {
             const cellStyle = colorGradient(value, 80)
             return <div style={cellStyle}>{value}</div>;
           },
-          sortType: 'basic', 
-        sorted: true, 
-        sortedDesc: false,
+          sortType: (a, b, column) => {
+            const aNum = parseFloat(a.original[column.accessor]);
+            const bNum = parseFloat(b.original[column.accessor]);
+            return aNum > bNum ? 1 : aNum < bNum ? -1 : 0;
+          },
       },
       {
-        Header: 'D_2',
+        Header: 'd_2',
         accessor: 'd_02',
         Cell: ({ value }) => {
           const cellStyle = colorGradient(value, 80)
           return <div style={cellStyle}>{value}</div>;
         },
-        sortType: 'basic', 
-        sorted: true, 
-        sortedDesc: false, 
+        sortType: (a, b, column) => {
+            const aNum = parseFloat(a.original[column.accessor]);
+            const bNum = parseFloat(b.original[column.accessor]);
+            return aNum > bNum ? 1 : aNum < bNum ? -1 : 0;
+          }, 
       },
       {
         Header: 'D_1',
@@ -144,20 +164,38 @@ function Table() {
           const cellStyle = colorGradient(value, 80)
           return <div style={cellStyle}>{value}</div>;
         },
-        sortType: 'basic', 
-        sorted: true, 
-        sortedDesc: false, 
+        sortType: (a, b, column) => {
+            const aNum = parseFloat(a.original[column.accessor]);
+            const bNum = parseFloat(b.original[column.accessor]);
+            return aNum > bNum ? 1 : aNum < bNum ? -1 : 0;
+          },
       },
+      {
+        Header: 'Calcul',
+        accessor: row => {
+          let count = 0;
+          let sum = 0;
+          let max = 0;
+          for (const key in row) {
+            if (/^d_\d+$/.test(key)) {
+              count++;
+              sum += Number(row[key]) || 0;
+              max = Math.max(max, Number(row[key]) || 0);
+            }
+          }
+          return (sum/(count*max));
+        }
+      }
+      
+      
     ],
     []
   );
 
- 
-  const tableInstance = useTable({ columns, data }, useSortBy);
-
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    tableInstance;
-
+  const tableInstance = useTable({ columns, data }, useFilters ,useSortBy);
+      const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+      tableInstance;
+  console.log(headerGroups[0].headers.length-4);
   return (
     <table {...getTableProps()}>
       <thead>
@@ -166,6 +204,7 @@ function Table() {
             {headerGroup.headers.map((column) => (
               <th {...column.getHeaderProps(column.getSortByToggleProps())}>
               {column.render('Header')}
+                         
               </th>
             ))}
           </tr>
